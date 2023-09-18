0257fbec177ab7d13e5677d806db03a0b9381a7c
esayan_well <esayan_well@mail.ru>
2023-09-18 16:37:20 +0200

    add folders for mvc

---
+++ InstProfile/InstProfile/Controllers/InstProfileViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstProfile
+//
+//  Created by Владимир Есаян on 18.09.2023.
+//
+
+import UIKit
+
+class InstProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstProfile/InstProfile/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  InstProfile
+//
+//  Created by Владимир Есаян on 18.09.2023.
+//
+
+import Foundation
+
+struct Post {
+    var postImage: String?
+    var Posttitle: String?
+}
